05fe00e53be4806643a7c983a88365006bbc98f0
Bobby Hutter <bobby-h@Bobbys-MacBook-Pro.local>
2021-12-22 17:24:09 -0500

    defined with statment resolving issue #3

--- class-based-context-managers.py
+++ class-based-context-managers.py
@@ -1,3 +1,14 @@
-# Write your code below: 
-
-
+# Checkpoint #1
+class PoemFiles:
+  def __init__(self):
+    # Checkpoint #2
+    print('Creating Poems!')
+  # Checkpoint #3
+  def __enter__(self):
+    print('Opening poem file')
+  # Checkpoint #4
+  def __exit__(self, *exc):
+      print('Closing poem file')
+# Checkpoint #5
+with PoemFiles() as manager:
+  print('Hope is the thing with feathers.')
