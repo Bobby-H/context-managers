b33b79787704ebcfc76e4fe5c7c25763bcc4cb0a
Bobby Hutter <bobby-h@Bobbys-MacBook-Pro.local>
2021-12-22 20:04:22 -0500

    CP4: successfully ran class argument resolving issue #4

--- class-based-context-managers-2.py
+++ class-based-context-managers-2.py
@@ -1,18 +1,18 @@
-# Checkpoint #1
 class PoemFiles:
-    def __init__(self, poem_file, mode):
-        print('Starting up a poem context manager')
-        self.poem_file = poem_file
-        self.mode = mode
-# Checkpoint #2
-    def __enter__(self):
-        print('Opening poem file')
-        self.opened_poem_file = open(self.file, self.mode)
-        return self.opened_poem_file
-# Checkpoint #3
-    def __exit__(self, *exc):
-        print('Closing poem file')
-        self.opened_poem_file.close()
+  
+  def __init__(self, poem_file, mode):
+    print('Starting up a poem context manager')
+    self.file = poem_file
+    self.mode = mode
 
-# with PoemFiles('poem.txt', 'w') as open_poem_file:
-#    open_poem_file.write('Hope is the thing with feathers')
+  def __enter__(self):
+    print('Opening poem file')
+    self.opened_poem_file = open(self.file, self.mode)
+    return self.opened_poem_file
+  
+  def __exit__(self, *exc):
+    print('Closing poem file')
+    self.opened_poem_file.close()
+
+with PoemFiles('poem.txt', 'w') as open_poem_file:
+   open_poem_file.write("Hope is the thing with feathers")
