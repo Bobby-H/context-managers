8df0c66fff599b124a7b3f890896299d82980dbb
Bobby Hutter <bobby-h@Bobbys-MacBook-Pro.local>
2021-12-22 19:57:22 -0500

    CP2: wrote enter method

--- class-based-context-managers-2.py
+++ class-based-context-managers-2.py
@@ -1,8 +1,14 @@
 # Checkpoint #1
 class PoemFiles:
-  def __init__(self, poem_file, mode):
-    print('Starting up a poem context manager')
-    self.poem_file = poem_file
-    self.mode = mode
+    def __init__(self, poem_file, mode):
+        print('Starting up a poem context manager')
+        self.poem_file = poem_file
+        self.mode = mode
+# Checkpoint #2
+    def __enter__(self):
+        print('Opening poem file')
+        self.opened_poem_file = open(self.file, self.mode)
+        return self.opened_poem_file
+
 # with PoemFiles('poem.txt', 'w') as open_poem_file:
 #    open_poem_file.write('Hope is the thing with feathers')
