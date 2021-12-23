c57f8dfa0d26e1d08d42575828951efd16b2a817
Bobby Hutter <bobby-h@Bobbys-MacBook-Pro.local>
2021-12-22 19:01:42 -0500

    CP1: initialized PoemFiles class

--- class-based-context-managers-2.py
+++ class-based-context-managers-2.py
@@ -1,7 +1,8 @@
-#Write your code below!
-
-
-
-
-
-
+# Checkpoint #1
+class PoemFiles:
+  def __init__(self, poem_file, mode):
+    print('Starting up a poem context manager')
+    self.poem_file = poem_file
+    self.mode = mode
+# with PoemFiles('poem.txt', 'w') as open_poem_file:
+#    open_poem_file.write('Hope is the thing with feathers')
