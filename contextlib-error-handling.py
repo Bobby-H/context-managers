f53ff3a2e8f5c322a225acecc10340e7c2f1a233
Bobby Hutter <bobby-h@Bobbys-MacBook-Pro.local>
2021-12-24 16:59:28 -0500

    Added exception handler for attribute errors

--- contextlib-error-handling.py
+++ contextlib-error-handling.py
@@ -6,13 +6,14 @@ def poem_files(file, mode):
   open_poem_file = open(file, mode)
   try:
     yield open_poem_file
-  #Write your code below: 
+  # Checkpoint #1
+  except AttributeError as e:
+    print(e)
 
   finally:
     print('Closing File')
     open_poem_file.close()
 
-# with poem_files('poem.txt', 'a') as opened_file:
-#  print('Inside yield')
-#  opened_file.sign('Buzz is big city. big city is buzz.')
-
+with poem_files('poem.txt', 'a') as opened_file:
+ print('Inside yield')
+ opened_file.sign('Buzz is big city. big city is buzz.')
